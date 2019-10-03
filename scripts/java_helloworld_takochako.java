18ef2b6a9ae2d695e862f31fc83e772f2ae5140e
takochako <32885729+takochako@users.noreply.github.com>
2019-10-02 22:03:24 -0400

    Heck

---
+++ scripts/java_helloworld_takochako.java
@@ -0,0 +1,14 @@
+/* 
+*  LANGUAGE: Java
+*  ENV: Java
+*  AUTHOR: takochako
+*  GITHUB: https://github.com/takochako
+*/ 
+
+public class java_helloworld_takochako {
+
+    public static void main(String[] args) {
+        System.out.println("Hello hecking world.");
+    }
+
+}
